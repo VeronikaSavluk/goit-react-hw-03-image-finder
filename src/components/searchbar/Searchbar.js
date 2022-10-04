5d9827625e71e6ccfca36e7a6ef88db3e2aa756e
Veronika Savluk <s_veronica@ukr.net>
2022-10-04 17:19:37 +0300

    Create Searchbar.js

---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+class SearchBar extends Component{
+    state = {
+
+    }
+    render() {
+        return (
+            <header class="searchbar">
+  <form class="form">
+    <button type="submit" class="button">
+      <span class="button-label">Search</span>
+    </button>
+
+    <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+        )
+    }
+}
+
+export default SearchBar;
