1b67edebc7f9ae5c3cedf756e1637001efa66751
Veronika Savluk <s_veronica@ukr.net>
2022-10-04 17:21:38 +0300

    Create Modal.js

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Modal extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Modal;
