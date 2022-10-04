68203e4bdcc9cc1bf61c7fddca4a726d7611fe70
Veronika Savluk <s_veronica@ukr.net>
2022-10-04 17:19:57 +0300

    Create ImageGallery.js

---
+++ src/components/imagegallery/ImageGallery.js
@@ -0,0 +1,10 @@
+import ImageGalleryItem from "components/imagegalleryitem/ImageGalleryItem";
+const ImageGallery = () => {
+    return (
+        <ul class="gallery">
+{ImageGalleryItem}
+        </ul>
+    )
+}
+
+export default ImageGallery;
