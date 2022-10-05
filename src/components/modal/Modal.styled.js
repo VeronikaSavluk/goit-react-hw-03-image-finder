0f4d8c4e3781573ff753ce494beaef1b1d418d26
Veronika Savluk <s_veronica@ukr.net>
2022-10-05 17:40:53 +0300

    Update Modal.styled.js

--- src/components/modal/Modal.styled.js
+++ src/components/modal/Modal.styled.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Image = styled.div`
+export const LargeImage = styled.div`
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
 &:before {
