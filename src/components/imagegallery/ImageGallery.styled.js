c03123c924b8c41222a0c9aef3781e1e31a3a2d5
Veronika Savluk <s_veronica@ukr.net>
2022-10-06 21:36:11 +0300

    Update ImageGallery.styled.js

--- src/components/imagegallery/ImageGallery.styled.js
+++ src/components/imagegallery/ImageGallery.styled.js
@@ -2,13 +2,12 @@ import styled from "@emotion/styled";
 
 export const List = styled.ul`
   display: grid;
+  justify-items: center;
+  align-items: center;
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
-  margin-top: 0;
-  margin-bottom: 0;
+  margin: 0 auto;
   padding: 0;
   list-style: none;
-  margin-left: auto;
-  margin-right: auto;
 `;
