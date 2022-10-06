06a845da4f31274a647ffb382108b177e5cf58fa
Veronika Savluk <s_veronica@ukr.net>
2022-10-06 03:05:25 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,21 @@ import ImageGallery from './imagegallery';
 import Modal from './modal/Modal';
 import { Component } from 'react';
 import { Section } from './App.styled';
-import axios from 'axios';
-import { BASE_URL, encodeURIComponent } from '../base/constants';
+
 class App extends Component {
   PER_PAGE = 12;
   state = {
-    images: [],
     query: '',
-    page: 1,
   }
 
-async componentDidMount() {
-  try {
-    const URL = `${BASE_URL}&${encodeURIComponent}&q=${this.query}&page=${this.page}&per_page=${this.PER_PAGE}`;
-    console.log(URL);
-    const {data} = await axios.get(URL);
-    return data;
-  } catch (error) {
-console.error(error);
-}
+  handleSearchForm = (query) => {
+    this.setState({query});
 }
   render() {
     const { query } = this.state;
     return (
       <Section>
-        <SearchBar onSearch={query} />
+        <SearchBar onSubmit={this.handleSearchForm} />
         <ImageGallery query={query} />
         <Modal />
       </Section>
