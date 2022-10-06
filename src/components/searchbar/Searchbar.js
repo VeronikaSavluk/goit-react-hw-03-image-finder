8cc3ca123235418c2ed152ceabe1751246906a66
Veronika Savluk <s_veronica@ukr.net>
2022-10-06 03:03:48 +0300

    Update Searchbar.js

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,26 +1,44 @@
 import { Component } from 'react';
+import { Formik } from 'formik';
+import { ImSearch } from 'react-icons/im';
 import { Header, SearchForm, Button, Label, Input } from './Searchbar.styled';
 
 class SearchBar extends Component{
-  state = {
+  initialValues = {
     query: '',
   }
+state = this.initialValues;
 
+  hendleSubmit = (values, { resetForm }) => {
+    const searchQuery = values.query.split(" ").join("+");
+    if (searchQuery !== '') {
+      // values.query = searchQuery;
+      this.props.onSubmit(searchQuery);
+      resetForm();
+    };
+  }
   render() {
     return (
-    <Header>
-      <SearchForm>
-        <Button type="submit">
-          <Label>Search</Label>
-        </Button>
-        <Input
-        type="text"
-        autocomplete="off"
-        autofocus
-        placeholder="Search images and photos"
-        />
-      </SearchForm>
-    </Header>
+      <Header>
+        <Formik initialValues={this.initialValues}
+          onSubmit={this.hendleSubmit}
+        >
+          <SearchForm>
+            <Button type="submit">
+              <ImSearch />
+              <Label>Search</Label>
+            </Button>
+            <Input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+              name="query"
+              required
+            />
+          </SearchForm>
+        </Formik>
+      </Header>
     )
   }
 }
