660699e8d14eaa786d56cbead178f070783f8dcb
Veronika Savluk <s_veronica@ukr.net>
2022-10-08 01:45:23 +0300

    Update ImageGalleryItem.styled.js

--- src/components/imagegalleryitem/ImageGalleryItem.styled.js
+++ src/components/imagegalleryitem/ImageGalleryItem.styled.js
@@ -7,7 +7,7 @@ export const Item = styled.li`
 `;
 
 export const Image = styled.img`
-  width: 100%;
+  width: 350px;
   height: 260px;
   object-fit: cover;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
