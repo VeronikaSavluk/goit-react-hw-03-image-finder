60dea2a64c5e1e649bb7db749db8489c6f11ed90
Veronika Savluk <s_veronica@ukr.net>
2022-10-08 01:21:26 +0300

    Update ImageGallery.js

--- src/components/imagegallery/ImageGallery.js
+++ src/components/imagegallery/ImageGallery.js
@@ -1,11 +1,14 @@
 import ImageGalleryItem from "components/imagegalleryitem";
 import { List } from './ImageGallery.styled';
 
-const ImageGallery = ({images}) => {
+const ImageGallery = ({ images, onSelect }) => {
+
     return (
         <List>
-            {images && images.map(({id, tags, webformatURL}) =>
-                <ImageGalleryItem key={id} tag={tags} url={webformatURL} />
+            {images && images.map(({id, tags, webformatURL, largeImageURL}) =>
+                <ImageGalleryItem key={id} tag={tags} url={webformatURL} onClick={() => onSelect(largeImageURL)}>
+                { largeImageURL }
+                </ImageGalleryItem>
                 )}
         </List>
     )
