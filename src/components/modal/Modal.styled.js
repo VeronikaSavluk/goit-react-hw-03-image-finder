f459b9fa8137c78f64d22b817443c321bb3f82d8
Veronika Savluk <s_veronica@ukr.net>
2022-10-08 01:17:49 +0300

    Update Modal.styled.js

--- src/components/modal/Modal.styled.js
+++ src/components/modal/Modal.styled.js
@@ -1,9 +1,6 @@
 import styled from '@emotion/styled';
 
-export const LargeImage = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
-&:before {
+export const Backdrop = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -12,7 +9,11 @@ export const LargeImage = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1200;
-}
+  background-color: rgba(4, 59, 92, .9);
+`;
+
+export const Content = styled.div`
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
+  padding: 24px;
 `;
